e84fce25add3e0d860f30238900654d94e626738
Stalemated <52725146+Stalemated@users.noreply.github.com>
2026-07-08 16:14:27 -0600

    Fix: 1. Filtro por autor y género (10%)

--- src/main/java/com/example/parcial/parcial2/services/BookService.java
+++ src/main/java/com/example/parcial/parcial2/services/BookService.java
@@ -3,6 +3,7 @@ package com.example.parcial.parcial2.services;
 import com.example.parcial.parcial2.domain.dtos.BookRequestDto;
 import com.example.parcial.parcial2.domain.dtos.GenreCountDto;
 import com.example.parcial.parcial2.domain.entities.Book;
+import com.example.parcial.parcial2.domain.entities.Genre;
 import com.example.parcial.parcial2.repositories.BookRepository;
 import org.springframework.stereotype.Service;
 
@@ -26,8 +27,17 @@ public class BookService {
         return null;
     }
 
-    public List<Book> getAllBooks(String author, String genre) {
-        return null;
+    public List<Book> getAllBooks(String author, String genreStr) {
+        if (author != null && genreStr != null) {
+            Genre genre = Genre.valueOf(genreStr.toUpperCase());
+            return bookRepository.findByAuthorAndGenre(author, genre);
+        } else if (author != null) {
+            return bookRepository.findByAuthor(author);
+        } else if (genreStr != null) {
+            Genre genre = Genre.valueOf(genreStr.toUpperCase());
+            return bookRepository.findByGenre(genre);
+        }
+        return bookRepository.findAll();
     }
 
     public Book updateBook(UUID id, BookRequestDto dto) {
